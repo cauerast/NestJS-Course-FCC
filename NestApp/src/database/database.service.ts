19435b88fce6857c5a775de87e2d37b5b8a7490f
cauerast <cauerast@gmail.com>
2026-07-09 15:39:03 -0300

    update

--- NestApp/src/database/database.service.ts
+++ NestApp/src/database/database.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
-    const adapter = new PrismaPg({connectionString: process.env.DATABASE_URL});
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+    const adapter = new PrismaPg(pool);
+
     super({ adapter });
   }
 
