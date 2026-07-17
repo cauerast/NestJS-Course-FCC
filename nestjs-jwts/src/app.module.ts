eda7ce3e5329eacd7b66baab8d7f87e82de89072
cauerast <cauerast@gmail.com>
2026-07-17 00:39:49 -0300

    reflecting guards in public rotes

--- nestjs-jwts/src/app.module.ts
+++ nestjs-jwts/src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './common/guards';
 
 @Module({
   imports: [AuthModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule {}
